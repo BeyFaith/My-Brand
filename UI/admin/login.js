e982adb19ed3872f19f521a0ae3cf4e40e01522d
BeyFaith <fryumugabe@gmail.com>
2020-08-08 13:13:27 +0200

    add admin page

---
+++ UI/admin/login.js
@@ -0,0 +1,25 @@
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+
+form.addEventListener('submit',(e) => {
+  e.preventDefault()
+
+
+  if (email.value === '' || email.value == null) {
+    document.getElementById('email-error').innerHTML = "Email is required"
+    document.getElementById('email-error').style.color = "red"
+  }
+  if (password.value === '' || password.value == null) {
+    document.getElementById('password-error').innerHTML = "Password is required"
+    document.getElementById('password-error').style.color = "red"
+  }
+  
+  else{
+    alert( 'Dear User, thanks for signing in');
+    form.reset ()
+}
+  
+})
